e08370787a8887c318c19ddde7ce2a79338533db
bygabrielamendes <gabriela_m_ferreira@outlook.com>
2026-02-02 23:50:56 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
         <input 
           type="number" 
           className="input-numero"
-          value={numero} 
           onChange={lidarComMudanca} 
           placeholder="Ex: 7"
         />
